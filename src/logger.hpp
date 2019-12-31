57920223490a9053328f04b0800488443feef565
iassasin <iassasin@yandex.ru>
2019-12-31 14:57:54 +0300

    add custom log for exceptions and boost::error_code

--- src/logger.hpp
+++ src/logger.hpp
@@ -5,18 +5,39 @@
 #ifndef LOGTRACTOR_LOGGER_HPP
 #define LOGTRACTOR_LOGGER_HPP
 
+#include <type_traits>
 #include <iostream>
+#include <boost/system/error_code.hpp>
+#include <exception>
 
 class Logger {
+private:
+	template<typename T>
+	void logArg(std::ostream &stream, T &&arg) {
+		using DT = std::remove_reference_t<T>;
+
+		if constexpr (std::is_base_of_v<boost::system::error_code, DT>) {
+			const boost::system::error_code &error = arg;
+			stream << error.message() << " [" << error.category().name() << ":" << error.value() << "]";
+		} else if constexpr (std::is_base_of_v<std::exception, DT>) {
+			const std::exception &ex = arg;
+			stream << "[exception] " << ex.what();
+		} else {
+			stream << arg;
+		}
+	}
 public:
 	template<typename ...Args>
 	void log(Args && ...args) {
-		(std::cout << ... << std::forward<decltype(args)>(args)) << std::endl;
+		(logArg(std::cout, std::forward<decltype(args)>(args)), ...);
+		std::cout << std::endl;
 	}
 
 	template<typename ...Args>
 	void error(Args && ...args) {
-		std::cerr << "[ERROR] " << (std::forward<decltype(args)>(args), ...) << std::endl;
+		std::cerr << "[ERROR] ";
+		(logArg(std::cerr, std::forward<decltype(args)>(args)), ...);
+		std::cerr << std::endl;
 	}
 
 	template<typename ...Args>
